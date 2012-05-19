2231a688e17c9fde3d54781c99d8712e5b2e4f94
Jérôme Mahuet <rydgel@ymail.com>
2012-05-19 22:36:43 +0200

    Preparing Heroku migration

---
+++ app.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+
+configure do
+  set :public_folder, Proc.new { File.join(root, "_site") }
+  # Last modification time for browser cache
+  @last_mod_time = Time.now
+end
+
+before do
+  headers "X-UA-Compatible" => "IE=Edge,chrome=1"
+  expires 300, :public, :must_revalidate
+  last_modified(@last_mod_time)
+end
+
+get '/' do
+  send_file('_site/index.html')
+end
+
+get '/*' do
+  file_path = '_site/' + params[:splat].join('/') + '/index.html'
+  if FileTest.exist?(file_path)
+    send_file(file_path)
+  end
+  halt 404
+end
+
+not_found do
+  send_file('_site/404.html')
+end
---
+++ config/unicorn.rb
@@ -0,0 +1,6 @@
+# amount of unicorn workers to spin up
+worker_processes 4
+# restarts workers that hang for 30 seconds
+timeout 30
+
+# http://unicorn.bogomips.org/ for more.
