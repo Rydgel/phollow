0d42113455d6622039fead8843d86f1f900f5431
Jérôme Mahuet <rydgel@ymail.com>
2012-06-20 11:38:27 +0200

    unicorn tuning

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 # amount of unicorn workers to spin up
-worker_processes 4
+worker_processes 16
 # restarts workers that hang for 30 seconds
-timeout 30
+timeout 2
 preload_app true
 # http://unicorn.bogomips.org/ for more.
