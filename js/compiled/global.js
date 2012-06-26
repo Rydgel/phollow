2644266cbb58ae5e0ea2b8b0832bc29a4b8338f3
Jérôme Mahuet <rydgel@ymail.com>
2012-06-26 02:40:19 +0200

    official Twitter JS removed

--- js/compiled/global.js
+++ js/compiled/global.js
@@ -39,8 +39,38 @@ a){var b=F.exec(a);b&&(b[1]=(b[1]||"").toLowerCase(),b[3]=b[3]&&new RegExp("(?:^
 
 })( jQuery );
 
+var clic, id;
+
+clic = function(dom_id, width, height) {
+  if (width == null) {
+    width = 500;
+  }
+  if (height == null) {
+    height = 300;
+  }
+  return dom_id.onclick = function() {
+    window.open(dom_id.href, "", "width=" + width + ", height=" + height);
+    return false;
+  };
+};
+
+id = function(dom_id) {
+  return document.getElementById(dom_id);
+};
+
+window.onload = function() {
+  var facebook_id, tweet_id;
+  tweet_id = id("tweet");
+  facebook_id = id("facebook");
+  if (tweet_id) {
+    clic(tweet_id);
+  }
+  if (facebook_id) {
+    return clic(facebook_id, 640, 280);
+  }
+};
+
 $(function() {
-  $('h2.entry-title').fitText(1);
   $('h2, h3, h4').fitText(1.3);
   $('.fullsizestuff').fitText(0.65);
 });
