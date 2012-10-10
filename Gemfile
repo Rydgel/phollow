c3d060478636eec4c307eeab877e4a42c1407a59
Jérôme Mahuet <jerome.mahuet@gmail.com>
2012-10-11 00:13:50 +0200

    Removing Ruby

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source 'http://rubygems.org'
-
-ruby '1.9.3'
-gem "sinatra"
-gem "unicorn"
-gem "haml"
-gem "sass"
-gem "newrelic_rpm"
--- config/unicorn.rb
+++
@@ -1,7 +0,0 @@
-# amount of unicorn workers to spin up
-worker_processes 7
-# restarts workers that hang for 30 seconds
-timeout 10
-preload_app true
-
-# http://unicorn.bogomips.org/ for more.
